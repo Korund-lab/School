5ca932866b1167303aaf54277d668bb7825ade27
Matheo <104639276+Korund-lab@users.noreply.github.com>
2022-10-01 14:09:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 package actions;
 
 import gui.Gui;
+import clocks.GameClock;
 
 public class Main
 {
     public void main()
     {
         Gui g = new Gui();
+        GameClock gc = new GameClock();
         
         g.create();
+        gc.start();
     }
 }
