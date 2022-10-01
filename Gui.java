bd669213f9f0815af6826a538209c29d2c6dcb9b
Matheo <104639276+Korund-lab@users.noreply.github.com>
2022-10-01 14:18:27 +0200

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,5 +1,7 @@
 package gui;
+
 import javax.swing.*;
+import actions.KeyHandler;
 
 public class Gui 
 {
@@ -17,6 +19,7 @@ public class Gui
         jf.setLocationRelativeTo(null);
         jf.setLayout(null);
         jf.setResizable(false);
+        jf.addKeyListener(new KeyHandler());
         
         d = new Draw();
         d.setBounds(0,0,width,height);
